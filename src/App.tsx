d10ca9e65ac1c7ada343671f2e19f473d4192ab2
Jaewoo ahn <ahnjaewoo3042@gmail.com>
2022-04-22 19:59:07 +0900

    add : data model

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Todos from "./components/Todos";
+import Todo from "./models/todos";
 
 function App() {
+  const todos = [new Todo("Learn React"), new Todo("Learn typescript")];
   return (
     <div>
-      <Todos items={["Learn React", "Learn typescript"]} />
+      <Todos items={todos} />
     </div>
   );
 }

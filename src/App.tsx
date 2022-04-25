285b7706865835625cb05bd4d32d236b81e9d669
Jaewoo ahn <ahnjaewoo3042@gmail.com>
2022-04-25 19:39:34 +0900

    add : state with type

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
+import {useState} from "react";
+
 import NewTodo from "./components/NewTodo";
 import Todos from "./components/Todos";
 import Todo from "./models/todos";
 
 function App() {
-  const todos = [new Todo("Learn React"), new Todo("Learn typescript")];
+  const [todos, setTodos] = useState<Todo[]>([]);
 
-  const AddTodoHandler = (todoText: string) => {};
+  const AddTodoHandler = (todoText: string) => {
+    const newTodo = new Todo(todoText);
+
+    setTodos((prevTodos) => {
+      return prevTodos.concat(newTodo);
+    });
+  };
   return (
     <div>
       <NewTodo onAddTodo={AddTodoHandler} />
